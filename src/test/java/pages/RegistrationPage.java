35bbd5ca9d6ff2f2f36de3a21fb45f5a6dced575
Jasmina <101935097+JasminaArslanova@users.noreply.github.com>
2022-08-07 17:03:58 +0300

    Update RegistrationPage.java

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -1,7 +1,9 @@
 package pages;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
+
 public class RegistrationPage {
+
     public RegistrationPage(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
