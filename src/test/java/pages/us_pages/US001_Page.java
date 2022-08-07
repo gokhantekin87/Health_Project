1cd868f8949f97a730eb9e09e5007a2f9e5debcd
Erkam USLU <100351822+USLUERKAM@users.noreply.github.com>
2022-08-07 17:01:03 +0300

    8/7/2022erkam

---
+++ src/test/java/pages/us_pages/US001_Page.java
@@ -0,0 +1,42 @@
+package pages.us_pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class US001_Page {
+    public US001_Page(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//a[@aria-haspopup='true']")
+    public WebElement manIcon;
+
+    @FindBy(xpath = "//span[text()='Register']")
+    public WebElement Register;
+
+    @FindBy(xpath = "//input[@name='ssn']")
+    public WebElement SSNBox;
+
+    @FindBy(xpath = "//input[@name='firstName']")
+    public WebElement firstNameBox;
+
+    @FindBy(xpath = "//input[@name='lastName']")
+    public WebElement lastName;
+
+    @FindBy(xpath="(//div[@class='invalid-feedback'])[1]")
+    public WebElement SSNHataMesaji;
+
+    @FindBy(xpath="//input[@id='firstName']")
+    public WebElement firstName;
+
+    @FindBy (xpath="(//div[@class='justify-content-center row'])[2]")
+    public WebElement bosAlan;
+
+    @FindBy(xpath="//input[@name='firstName']")
+    public WebElement firstNameHataMesaji;
+
+    @FindBy(xpath="//*[text()='Your LastName is required.']")
+    public WebElement lastNameHataMesasji;
+}
--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         },
         features = "./src/test/resources/features",
         glue = "stepdefinitions",
-        tags = "@",
+        tags = "@tum",
         dryRun = false
 )
 public class TestRunner {
