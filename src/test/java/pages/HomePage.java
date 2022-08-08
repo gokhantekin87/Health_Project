0b7428643d489038c864dc2e87786d7f184009a4
Bilal Ergin <bilalergin1587@gmail.com>
2022-08-08 20:25:09 +0300

    HomePage

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,5 +1,24 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class HomePage {
+    public HomePage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+
+    }
+    @FindBy(xpath = "//a[@class='d-flex align-items-center dropdown-toggle nav-link']//*[name()='svg']")
+    public WebElement accountMenu;
+    @FindBy(xpath = "//*[text()='Sign in']")
+    public WebElement signInButton;
+    @FindBy(xpath = "//*[text()='Register']")
+    public WebElement registerButton;
+    @FindBy(xpath = "//a[@class='appointment-btn scrollto']")
+    public WebElement makeAnAppointment;
     // Sign in den onceki sayfa
 }
+
+
