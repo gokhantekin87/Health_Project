2e80baee2bcb9af6d37ef02aec549ec84865e1dd
Jasmina Arslanova <zhasmina.arslanova@gmail.com>
2022-08-08 17:56:41 +0300

    Login Page ve Login methodu eklendi 08/08/2022

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -9,7 +9,7 @@ public class LoginPage {
     public LoginPage(){PageFactory.initElements(Driver.getDriver(),this);}
 
  @FindBy(xpath = "//*[@id='account-menu']") public WebElement accountMenu;
- @FindBy(id = "login-item") public WebElement loginButton; //SignIn sayfasini acma butonu
+ @FindBy(id = "login-item") public WebElement signIn; //SignIn sayfasini acma butonu
  @FindBy(id = "username") public WebElement usernameTextbox;
  @FindBy(id = "password") public WebElement passwordTextbox;
  @FindBy(xpath = "(//span[text()='Sign in'])[3]") public WebElement signInButton; //SignIn sayfasindaki signin buttonu
