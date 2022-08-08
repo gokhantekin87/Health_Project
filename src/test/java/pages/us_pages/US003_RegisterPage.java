011721c288248c2e4117e8be71a4d291ef9986b0
Mine Boz <bozmine574@gmail.com>
2022-08-08 22:49:43 +0300

    v2

--- src/test/java/pages/us_pages/US003_RegisterPage.java
+++ src/test/java/pages/us_pages/US003_RegisterPage.java
@@ -21,23 +21,22 @@ public class US003_RegisterPage {
     @FindBy(xpath = "//input[@id='secondPassword']")
     public WebElement registerConfirmPasswordBox;
 
-    @FindBy(xpath = "//*[@style='background-color: rgb(255, 0, 0);']")
+    @FindBy(xpath = "(//li[@class='point'])[1]")
     public static WebElement passwordStrenght1;
 
-    @FindBy(xpath = "(//*[@style='background-color: rgb(153, 255, 0);'])[2]")
+    @FindBy(xpath = "(//li[@class='point'])[2]")
     public static WebElement passwordStrenght2;
 
-    @FindBy(xpath = "(//*[@style='background-color: rgb(255, 153, 0);'])[2]")
+    @FindBy(xpath = "(//li[@class='point'])[3]")
     public static WebElement passwordStrenght3;
 
-    @FindBy(xpath = "(//*[@style='background-color: rgb(153, 255, 0);'])[3]")
-    public static WebElement passwordStrength3;
-
-    @FindBy(xpath = "(//*[@style='background-color: rgb(153, 255, 0);'])[4]")
+    @FindBy(xpath = "(//li[@class='point'])[4]")
     public static WebElement passwordStrength4;
 
-    @FindBy(xpath = "(//*[@style='background-color: rgb(153, 255, 0);'])[5]")
+    @FindBy(xpath = "(//li[@class='point'])[5]")
     public static WebElement passwordStrength5;
+
+
 
 
 
