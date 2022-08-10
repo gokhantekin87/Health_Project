4da13e1b8a1618075f18fe63afc798032b62dfb4
Jasmina Arslanova <zhasmina.arslanova@gmail.com>
2022-08-10 23:56:08 +0300

    us009 10/08/2022

--- src/test/java/pages/staff_pages/Staff_SearchPatientPage.java
+++ src/test/java/pages/staff_pages/Staff_SearchPatientPage.java
@@ -1,4 +1,13 @@
 package pages.staff_pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class Staff_SearchPatientPage {
+
+    Staff_SearchPatientPage(){PageFactory.initElements(Driver.getDriver(),this);}
+
+    @FindBy(xpath = "//tbody")public WebElement paientTbody;
 }
--- src/test/java/pages/us_pages/US009_Page.java
+++ src/test/java/pages/us_pages/US009_Page.java
@@ -1,4 +1,20 @@
 package pages.us_pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class US009_Page {
+
+    public US009_Page(){PageFactory.initElements(Driver.getDriver(),this);}
+/*
+search-patient
+patient-update
+ */
+
+
+   // @FindBy(xpath = "//tbody")public WebElement paientTbody;
+
+
 }
