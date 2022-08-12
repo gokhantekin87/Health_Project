afbb502644fed1ab88720ab018548cae5339e193
Mine Boz <bozmine574@gmail.com>
2022-08-12 08:03:07 +0300

    12/08/2022

--- src/test/java/stepdefinitions/ui_stepdefinitions/US004_SingInPageStepDef.java
+++ src/test/java/stepdefinitions/ui_stepdefinitions/US004_SingInPageStepDef.java
@@ -7,7 +7,7 @@ import org.junit.Assert;
 import pages.us_pages.US_004_LoginPage;
 import utilities.Driver;
 
-public class US004_LoginPageStepDef {
+public class US004_SingInPageStepDef {
      US_004_LoginPage login4=new US_004_LoginPage();
 
 
